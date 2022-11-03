8bb1ea764ff8d1dca764cce2d2e01ddd8a6d0022
wisehero10 <godjw96@gmail.com>
2022-11-04 00:00:33 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/coubang/coubang/CoubangApplication.java
@@ -0,0 +1,13 @@
+package com.coubang.coubang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoubangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoubangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coubang/coubang/CoubangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coubang.coubang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoubangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
