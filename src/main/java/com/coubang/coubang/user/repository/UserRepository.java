f9a522c776e7202905c198c812d62016dc1a6564
wisehero10 <godjw96@gmail.com>
2022-11-05 22:46:55 +0900

    feat : 이메일로 유저 찾기 메소드 추가

--- src/main/java/com/coubang/coubang/user/repository/UserRepository.java
+++ src/main/java/com/coubang/coubang/user/repository/UserRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coubang.coubang.user.model.User;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByEmail(String email);
 }
