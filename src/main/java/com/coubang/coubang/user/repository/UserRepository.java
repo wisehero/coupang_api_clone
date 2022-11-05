396659cea088321792c71b9613d5d178112537fc
wisehero10 <godjw96@gmail.com>
2022-11-05 14:40:49 +0900

    feat : UserRepository 생성

---
+++ src/main/java/com/coubang/coubang/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.coubang.coubang.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.coubang.coubang.user.model.User;
+
+public interface UserRepository extends JpaRepository<Long, User> {
+}
